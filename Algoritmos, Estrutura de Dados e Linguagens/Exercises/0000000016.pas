c74ee2d6cfcb2f4521b95cb857b3b75edaf535b7
x86PHS <rique.souza06@icloud.com>
2026-01-22 23:01:55 -0300

    Renomeando arquivos java e c

--- Algoritmos, Estrutura de Dados e Linguagens/Exercises/0000000016.pas
+++
@@ -1,8 +0,0 @@
-PROGRAM ex0000000016;
-USES CRT,MATH;
-VAR cateto1,cateto2,hipotenusa : real;
-BEGIN
-    readln(cateto1);readln(cateto2);
-    hipotenusa := sqrt(power(cateto1,2) + power(cateto2,2));
-    writeln(hipotenusa:2:2);
-END.
